1bea2e79a8174fa4f36e58b1d47414f734615650
Michael Yang <135294530+gitLcat@users.noreply.github.com>
2023-11-06 16:42:59 -0800

    Update rplidar_s1.launch.py

--- launch/rplidar_s1.launch.py
+++ launch/rplidar_s1.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             node_executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/ttyTHS1',
                 'serial_baudrate': 256000,
                 'frame_id': 'laser',
                 'inverted': False,
